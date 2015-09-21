51b9fbef69c9dc6e96e5281200a4f46c74cf08a5
Guillaume Benzoni <guillaume.benzoni@laposte.net>
2015-09-21 16:15:28 +0200

    Gestion de la connexion des comptables : ok
    Nouvelle vue : v_sommaire_comptable
    Il faudra faire en sorte d'afficher ce qu'il faut sur cette vue
    Nouvelle variable $_SESSION["type"]  qui détermine si l'utilisateur connecté est "visiteur" ou "comptable"

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -13,15 +13,28 @@ switch($action){
 		$mdp = $_REQUEST['mdp'];
 		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
 		if(!is_array( $visiteur)){
+                    $visiteur = $pdo->getInfosComptable($login,$mdp);
+                    if(!is_array( $visiteur)){
 			ajouterErreur("Login ou mot de passe incorrect");
 			include("vues/v_erreurs.php");
 			include("vues/v_connexion.php");
+                    }
+                    else
+                    {
+                        $id = $visiteur['id'];
+			$nom =  $visiteur['nom'];
+			$prenom = $visiteur['prenom'];
+			connecter($id,$nom,$prenom);
+                        $_SESSION['type']="comptable";
+			include("vues/v_sommaire_comptable.php");
+                    }
 		}
 		else{
 			$id = $visiteur['id'];
 			$nom =  $visiteur['nom'];
 			$prenom = $visiteur['prenom'];
 			connecter($id,$nom,$prenom);
+                        $_SESSION['type']="visiteur";
 			include("vues/v_sommaire.php");
 		}
 		break;
--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -60,6 +60,15 @@ class PdoGsb{
 		$ligne = $rs->fetch();
 		return $ligne;
 	}
+        
+        public function getInfosComptable($login, $mdp){
+		$req = "select comptable.id as id, comptable.nom as nom, comptable.prenom as prenom from comptable 
+		where comptable.login='$login' and comptable.mdp='$mdp'";
+		$rs = PdoGsb::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+        
 
 /**
  * Retourne sous forme d'un tableau associatif toutes les lignes de frais hors forfait
